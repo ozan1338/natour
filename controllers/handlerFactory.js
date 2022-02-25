f1aa262e4cca443c607ea07d90fea329ae4c381c
ozan1338 <akhmadfauzan1338@gmail.com>
2022-02-25 16:31:43 +0700

    Fix rating Average and rating Quantity and Preventing Duplicate Review

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -71,7 +71,7 @@ const getAll = Model => catchAsync(async(req,res,next)=>{
     if (req.params.tourId) filter = {tour: req.params.tourId}
 
     const features = new APIFeatures(Model.find(filter), req.query).filter().sort().limitFields().paginate()
-    const doc = await features.query;
+    const doc = await features.query
 
     //send response
     res.status(200).json({
--- model/tourModel.js
+++ model/tourModel.js
@@ -34,7 +34,8 @@ const tourSchema = new mongoose.Schema({
         type: Number,
         default: 4.5,
         min: [1, "Rating must be above 1.0"],
-        max: [5, "Rating must be below 5.0"]
+        max: [5, "Rating must be below 5.0"],
+        set: val => Math.round(val * 10) / 10   
     },
     ratingsQuantity: {
         type: Number,
@@ -111,6 +112,10 @@ const tourSchema = new mongoose.Schema({
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
 })
+
+//tourSchema.index({price: 1})
+tourSchema.index({price: 1, ratingAverage: -1})
+tourSchema.index({slug: 1})
 
 tourSchema.virtual('durationWeeks').get(function() {
     return this.duration / 7;
